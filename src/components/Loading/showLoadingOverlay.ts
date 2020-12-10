bedf0d232615396a2b1529254615650ab7c7897b
levy <levy@codeandmore.pl>
2020-12-10 11:58:02 +0100

    Commit

--- src/components/Loading/showLoadingOverlay.ts
+++ src/components/Loading/showLoadingOverlay.ts
@@ -32,14 +32,106 @@ export class LoadingOverlayProxy implements LoadingOptions {
         }
     }
 
-    header: string;
-    message: string;
-    progressBuffer: number;
-    progressMessage: string;
-    progressPercent: number;
-    progressType: "determinate" | "indeterminate";
-    progressValue: number;
-    type: "spinner" | "progress";
+    get header() {
+        if (this.loading) {
+            return this.loading.header;
+        } else {
+            return undefined;
+        }
+    }
+
+    set header(header: string) {
+        if (this.loading) {
+            this.loading.header = header;
+        }
+    }
+
+    get message() {
+        if (this.loading) {
+            return this.loading.message;
+        } else {
+        }
+    }
+
+    set message(message: string) {
+        if (this.loading) {
+            this.loading.message = message;
+        } else {
+
+        }
+    }
+
+    get progressBuffer() {
+        if (this.loading) {
+            return this.loading.progressBuffer;
+        }
+    }
+
+    set progressBuffer(buffer: number) {
+        if (this.loading) {
+            this.loading.progressBuffer = buffer;
+        }
+    }
+
+    get progressMessage() {
+        if (this.loading) {
+            return this.loading.progressMessage;
+        }
+    }
+
+    set progressMessage(message: string) {
+        if (this.loading) {
+            this.loading.progressMessage = message;
+        }
+    }
+
+    get progressPercent() {
+        if (this.loading) {
+            return this.loading.progressPercent;
+        }
+    }
+
+    set progressPercent(progress: number) {
+        if (this.loading) {
+            this.loading.progressPercent = progress;
+        }
+    }
+
+    get progressType() {
+        if (this.loading) {
+            return this.loading.progressType;
+        }
+    }
+
+    set progressType(type) {
+        if (this.loading) {
+            this.loading.progressType = type;
+        }
+    }
+
+    get progressValue() {
+        if (this.loading) {
+            return this.loading.progressValue;
+        }
+    }
+
+    set progressValue(value) {
+        if (this.loading) {
+            this.loading.progressValue = value;
+        }
+    }
+
+    get type() {
+        if (this.loading) {
+            return this.loading.type;
+        }
+    }
+
+    set type(type) {
+        if (this.loading) {
+            this.loading.type = type;
+        }
+    }
 }
 
 export async function showLoadingOverlay(options: LoadingOptions) {
