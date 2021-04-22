a471bda146c1f4a02ff45991bc1430cb45d7adb7
levy <levy@codeandmore.pl>
2021-04-22 22:44:01 +0200

    Commit

--- src/components/ToggleLabels/ToggleLabels.tsx
+++ src/components/ToggleLabels/ToggleLabels.tsx
@@ -1,4 +1,4 @@
-import {Component, Element, h, Host, Prop} from "@stencil/core";
+import {Component, Element, Event, EventEmitter, h, Host, Listen, Prop, Watch} from "@stencil/core";
 
 @Component({
     tag: "ionx-toggle-labels",
@@ -7,38 +7,83 @@ import {Component, Element, h, Host, Prop} from "@stencil/core";
 })
 export class ToggleLabels {
 
+    @Element()
+    element: HTMLElement;
+
     @Prop()
     on: string;
 
     @Prop()
     off: string;
 
-    @Element()
-    private el: HTMLElement;
+    @Prop()
+    readonly: boolean;
+
+    @Prop()
+    disabled: boolean;
+
+    @Prop({mutable: true})
+    value: boolean;
+
+    @Event()
+    ionChange: EventEmitter<{value: boolean}>;
+
+    initialToggleState: {checked: boolean, disabled: boolean};
 
     private get toggle() {
-        return this.el.querySelector<HTMLIonToggleElement>("ion-toggle");
+        return this.element.querySelector<HTMLIonToggleElement>("ion-toggle");
     }
 
-    switchOn() {
-        this.toggle.checked = true;
+    switchToggle(state: "on" | "off") {
+        if (!this.disabled && !this.readonly) {
+            this.toggle.checked = state === "on";
+        }
     }
 
-    switchOff() {
-        this.toggle.checked = false;
+    @Listen("ionChange")
+    toggleChanged() {
+
+        const was = this.value;
+        this.value = this.toggle.checked;
+
+        if (was !== this.value) {
+            this.ionChange.emit({value: this.value});
+        }
+    }
+
+    @Watch("value")
+    valueChanged() {
+        if (this.toggle.checked !== this.value) {
+            this.toggle.checked = this.value;
+        }
+    }
+
+    @Watch("readonly")
+    @Watch("disabled")
+    syncToggle() {
+        const toggle = this.toggle;
+        toggle.disabled = this.readonly || this.disabled;
+    }
+
+    connectedCallback() {
+        this.initialToggleState = {checked: this.value, disabled: this.readonly || this.disabled};
     }
 
     render() {
-        return <Host>
+        return <Host class={{"ionx--interactive": !this.disabled && !this.readonly, "ionx--readonly": this.readonly}}>
 
-            <span class="off" onClick={() => this.switchOff()}>
+            {this.readonly && <span>{this.value ? this.on : this.off}</span>}
+
+            <span class="ionx--off" onClick={() => this.switchToggle("off")}>
                 {this.off && <span>{this.off}</span>}
                 <slot name="off"/>
             </span>
 
-            <slot/>
+            <slot>
+                <ion-toggle class="ionx--default-toggle" {...this.initialToggleState}/>
+            </slot>
 
-            <span class="on" onClick={() => this.switchOn()}>
+            <span class="ionx--on" onClick={() => this.switchToggle("on")}>
                 {this.on && <span>{this.on}</span>}
                 <slot name="on"/>
             </span>
