c95d377e9c31354dd7e10a6d76bdb32427f04ec5
levy <levy@codeandmore.pl>
2021-04-28 11:14:42 +0200

    Commit

--- modules/intlMessagesLoaderGeneratorPlugin.ts
+++ modules/intlMessagesLoaderGeneratorPlugin.ts
@@ -1,9 +1,11 @@
-export function intlMessagesLoaderGeneratorPlugin(loaderPath: string = "src/intl/loadIntlMessages.ts") {
+import {readdirSync} from "fs";
+
+export function intlMessagesLoaderGeneratorPlugin(jsonPath: string = "src/intl", loaderPath: string = "src/intl/loadIntlMessages.ts") {
     return {
         name: "intlMessagesLoaderGenerator",
         transform: (_sourceText: string, sourcePath: string) => {
             if (sourcePath.endsWith(loaderPath)) {
-                const locales = ["pl"];
+                const locales = readdirSync(jsonPath).filter(f => f.endsWith(".json")).map(f => f.replace(".json", ""));
                 return _sourceText.replace(/(switch \(locale\) \{)/, "$1" + locales.map(locale => `case "${locale}": return (await import("./${locale}.json")).default;\n`).join(""));
             }
         }
