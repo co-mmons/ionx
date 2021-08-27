809412e1c241807270446891444c92dc6b7618b6
Mark Chris Levy <levy@codeandmore.pl>
2021-08-27 19:16:05 +0200

    Commit

--- modules/Select/src/index.ts
+++ modules/Select/src/index.ts
@@ -2,5 +2,6 @@ export {SelectDividerItem} from "./SelectDividerItem";
 export {SelectGroupItem, SelectLazyGroupItem} from "./SelectGroupItem";
 export {SelectItem} from "./SelectItem";
 export {SelectOverlayProps} from "./SelectOverlayProps";
+export {SelectProps} from "./SelectProps";
 export {SelectValueItem, SelectValueItem as SelectOption} from "./SelectValueItem";
 export {showSelectOverlay} from "./showSelectOverlay";
